2c21b7c5d3cbde3975cc94296f0470f0fc9de951
Tzeyi <43532766+TY235@users.noreply.github.com>
2019-11-07 11:03:22 +0000

    First attempt to do Task 2 & 3

---
+++ task2.c
@@ -0,0 +1,9 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <semaphore.h>
+
+int main(){
+
+
+
+}
---
+++ task3.c
@@ -0,0 +1,7 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <semaphore.h>
+
+int main(){
+
+}
