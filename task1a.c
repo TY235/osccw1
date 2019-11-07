e0d2a9a7e54ea8605e6cf1affb6bb58ecf03b879
Tzeyi <43532766+TY235@users.noreply.github.com>
2019-11-07 11:01:59 +0000

    Completed Task 1 a & b (No more memory leak)

--- task1a.c
+++ task1a.c
@@ -16,13 +16,17 @@ int main(){
 	int turnAroundTime = 0;
 	struct process * oTemp;
 
+
+	// Display and all processes to the linked list
 	for(int i = 0; i < NUMBER_OF_JOBS; i++){
 		oTemp = generateProcess();
 		addLast(oTemp,&pHead,&pTail);
 	}	
 	
+	// Reset the pointer to the first node
 	pPointer = pHead;
 
+	// Run the linked list and calculate the response time and turnaround time
 	for(int j = 0; j < NUMBER_OF_JOBS; j++){
 		runNonPreemptiveJob((struct process*)pPointer->pData, &oStartTime, &oEndTime);	
 		responseTime = getDifferenceInMilliSeconds(((struct process*)(pPointer->pData))->oTimeCreated,oStartTime);
@@ -32,11 +36,18 @@ int main(){
 		printf("Process Id = %d, Previous Burst Time = %d, New Burst Time = %d, Response Time = %d, Turn Around Time = %d\n",((struct process*)(pPointer->pData))->iProcessId,((struct process*)(pPointer->pData))->iInitialBurstTime,((struct process*)(pPointer->pData))->iRemainingBurstTime,responseTime,turnAroundTime);
 		pPointer = pPointer->pNext; 
 	}
+
+	// Calculate the average response and turnaround time
 	printf("Average response time = %.6f\n",avgResponseTime/NUMBER_OF_JOBS);
 	printf("Average turn around time = %.6f\n",avgTurnAroundTime/NUMBER_OF_JOBS);
 
-	for(int k = 0; k < NUMBER_OF_JOBS; k++){
-		removeFirst(&pHead,&pTail);
+	// Reset the pointer to the first node
+	pPointer = pHead;
 
+	// Free and release all the node from the linked list
+	for(int k = 0; k < NUMBER_OF_JOBS; k++){
+		free(pPointer->pData);
+		pPointer = pPointer->pNext;
+		removeFirst(&pHead,&pTail);
 	}
 }
